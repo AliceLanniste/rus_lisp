062289f450ce0154be398ec3bb1d8a2341a23323
AliceLanniste <1399789151@qq.com>
2020-02-26 21:28:11 +0800

    added env

--- src/Ast.rs
+++ src/Ast.rs
@@ -1,8 +1,17 @@
 
+
 #[derive(Debug,Clone)]
 pub enum AstNode {
     Number(i64),
     Bool(bool),
     List(Vec<AstNode>),
-    
+    Func(SCallbe)
+}
+
+
+type SCallbe = fn(&[AstNode]) -> Result<AstNode, AstError>;
+
+[derive(Debug)]
+pub enum AstErr {
+   Msg(String),
 }
---
+++ src/env.rs
@@ -0,0 +1,26 @@
+pub mod Ast;
+use Ast::AstNode;
+
+pub struct env{
+   map: Vec<Hashmap<String,AstNode>>
+}
+
+impl env {
+    fn new() -> Self {
+        Self {
+            map: vec![Hashmap::new()]
+        }
+    }
+  
+    pub fn set(&mut self,name:String,value:AstNode)  {
+        self.env[0].insert(name,value)
+    }
+
+    pub fn set_global(&mut self,name:String,valeu:AstNode)  {
+        self.env.insert(name,AstNode)
+    }
+
+    pub fn get(&self,key:String) ->Option<AstNode>{
+        unimplemented!();
+    }
+}
